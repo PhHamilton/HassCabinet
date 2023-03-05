ae1c90add91cc57d33cc1e62afe4c877c11a1fda
Philip <philip.h.hamilton@gmail.com>
2023-03-05 15:57:07 +0100

    Added more settings tests

--- microcontroller/main/src/unit_test.cpp
+++ microcontroller/main/src/unit_test.cpp
@@ -37,31 +37,90 @@ void test_settings_handler_UpdateSettings(void)
 {
 
 	SettingsHandler settings;
+	uint16_t settingsMsg; 
+	uint8_t settingsVal;
+	uint8_t Expected;
 	/*
 		* Settings: 
 			Output: 1 
 			
-			Logging Frequency: 2 Hz
-			Enable Logging: 1 
-			Enable Fan Controller: 1
+				Logging Frequency: 2 Hz
+				Enable Logging: 1 
+				Enable Fan Controller: 1
+				
+
+				Blink: 1 Hz
+				Force on: 0 
+				Enable Led: 1 
+				
+
+				0001 1011 0101 0000  
+
+			Output: 2 
+
+				Logging Frequency: 3 Hz
+				Enable Logging: 1 
+				Enable Fan Controller: 0
+				
+
+				Blink: 0 Hz
+				Force on: 1 
+				Enable Led: 0 
 			
 
-			Blink: 1 Hz
-			Force on: 0 
-			Enable Led: 1 
+			0010 1110 0010 0000  
+
+			Output: 3
+			
+				Logging Frequency: 0 Hz
+				Enable Logging: 0 
+				Enable Fan Controller: 0
+				
+
+				Blink: 0 Hz
+				Force on: 0 
+				Enable Led: 0 
+				
+
+				0100 0000 0000 0000  
+
+			Output: 4 
+
+				Logging Frequency: 3 Hz
+				Enable Logging: 1 
+				Enable Fan Controller: 1
+				
+
+				Blink: 1 Hz
+				Force on: 1 
+				Enable Led: 1 
 			
 
-			0001 1110 0101 0000  
-
-
+			100 1110 0111 0000
 	*/ 
-	uint16_t settingsMsg = 0x1B50; 
+	settingsMsg = 0x1B50; 
 	settings.UpdateSettings(settingsMsg); 
-	uint8_t settingsVal = settings.GetOutputSettings(0); 
-
-	uint8_t Expected = 0xB5; 
+	settingsVal = settings.GetOutputSettings(0); 
+	Expected = 0xB5; 
 	TEST_CHECK_(settingsVal == Expected, "Testing GetOutputSettings(0): Expected :%d, actual %d", Expected, settingsVal);
 
+	settingsMsg = 0x2E20;
+	settings.UpdateSettings(settingsMsg); 
+	settingsVal = settings.GetOutputSettings(1); 
+	Expected = 0xE2; 
+	TEST_CHECK_(settingsVal == Expected, "Testing GetOutputSettings(1): Expected :%d, actual %d", Expected, settingsVal);
+
+	settingsMsg = 0x4000;
+	settings.UpdateSettings(settingsMsg); 
+	settingsVal = settings.GetOutputSettings(2); 
+	Expected = 0x00; 
+	TEST_CHECK_(settingsVal == Expected, "Testing GetOutputSettings(2): Expected :%d, actual %d", Expected, settingsVal);
+
+	settingsMsg = 0x8E70;
+	settings.UpdateSettings(settingsMsg); 
+	settingsVal = settings.GetOutputSettings(3); 
+	Expected = 0xE7; 
+	TEST_CHECK_(settingsVal == Expected, "Testing GetOutputSettings(3): Expected :%d, actual %d", Expected, settingsVal);
 
 }
 
