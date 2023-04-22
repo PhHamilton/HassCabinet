bc5491db13f9dfea305b54f8ffd664095fa5ceeb
Philip <philip.h.hamilton@gmail.com>
2023-04-22 11:38:58 +0200

    Added commands

--- python/HassBoxCommands.py
+++ python/HassBoxCommands.py
@@ -1,31 +1,49 @@
+FAN_OUTPUT = 8
 
 class HassBoxCommands()
 	def __init__(): 
+		stringBuilder = OutputStringBuilder()
+
+	def UpdateOutputSettings(self, outputNumber, settings): 
 		pass
 
-	def UpdateOutputSettings(self, output, settings): 
-		pass
-
-	def GetCurrentSettings(self): 
+	def GetCurrentOutputSettings(self, outputNumber): 
 		pass
 
 	def TurnOnFan(self): 
-		pass
+		return stringBuilder.ConvertToHexNumber(FAN_OUTPUT) 
 
 	def GetBoxTemperature(self): 
 		return "0xA000"
 
 	def GetBoxHumidity(self): 
-		pass
+		return "0xB000"
 
 	def TurnOnOutputNo(self, outputNumber): 
-		pass
+		#currentOutputState
+		#newoutputstate
+		return stringBuilder.ConvertToHexNumber(outputNumber)
 
 	def TurnOffOutputNo(self, outputNumber):
-		pass
+		#currentOutputState = Get output state! 
+		#newOutputState |= ~self.__shiftBit(outputNumber)
+
+		return stringBuilder.ConvertToHexNumber(outputNumber)
 
 	def TurnOnAllOutputs(self): 
 		return "0x000F"
 
 	def TurnOffAllOutputs(self): 
 		return "0x0000"
+
+class OutputStringBuilder(self): 
+	def __init__(self): 
+		pass
+	def ConvertToHexNumber(self, outputNo): 
+		return "0x000" + self.__numberToString(self.__shiftBit(outputNumber))
+
+	def __shiftBit(self, bitpos)
+		return 1 << bitpos
+
+	def __numberToString(number)
+		return string(number)
